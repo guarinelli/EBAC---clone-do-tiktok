1fb196325acba67637e9af565e216f207c1bf46d
ccmikaelli <guarinelli.cm@hotmail.com>
2023-06-16 21:28:05 -0300

    sidebar - tarefa 4
    o projeto rodando e com a sidebar pronta. Deve conter os ícones estilizados e os números abaixo.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,26 @@ function App() {
   return (
     <div className="App">
       <div className='app__videos'>
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        
+        <Video 
+          likes={111}
+          messages={222}
+          shares={333}
+          name='paulo'
+          description='Brecker o goleiro'
+          music='musica epica'
+          url='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z'
+        />
+          <Video 
+          likes={444}
+          messages={555}
+          shares={666}
+          name='celia'
+          description='bird olhando para a camera'
+          music='clap your hands'
+          url='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z'
+        />
+
+      
       </div>
     </div>
   );
